2e191d46730c831bde37af9415261e887f940a0a
EC2 Default User <ec2-user@ip-172-31-1-202.ap-south-1.compute.internal>
2025-04-16 15:29:02 +0000

    bulk update

---
+++ gittest/first_script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#printing
+echo "Enter some runtime variable"
+read runtimevar
+echo "below is the value that has entered"
+echo "run time value is $runtimevar"
+#awk -F: '{print $1}' /etc/passwd > /home/ec2-user/scripts/output
+
+
+
